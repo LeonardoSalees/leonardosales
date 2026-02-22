6a58cc8e136d6ac68ebe108d0b3b08013fa075b2
Leonardo Sales <leonardosales.silva12@gmail.com>
2026-02-21 21:04:40 -0300

    first commit portfolio

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,8 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Leonardo Sales | Full Stack Developer",
-  description: "Portfólio profissional de  Leonardo Sales, desenvolvedor Full Stack especialista em React, Next.js e Node.js.",
+  description:
+    "Portfólio profissional de  Leonardo Sales, desenvolvedor Full Stack especialista em React, Next.js e Node.js.",
 };
 
 export default function RootLayout({
@@ -25,24 +26,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-br" className="scroll-smooth"> {/* Adicionado scroll suave */}
+    <html lang="pt-br" className="scroll-smooth">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#0a0c10] text-white font-[family-name:var(--font-geist-sans)]`}
       >
-        <Header /> 
-        {/* Camada de Background Blobs */}
-        <div className="fixed inset-0 -z-10 overflow-hidden pointer-events-none">
-          {/* Bolha superior esquerda (Ciano) */}
-          <div className="absolute top-[-10%] left-[-10%] w-[500px] h-[500px] bg-brand-cyan/15 rounded-full blur-[120px] animate-blob" />
-          
-          {/* Bolha meio direita (Azul/Roxo) */}
-          <div className="absolute top-[30%] right-[-5%] w-[400px] h-[400px] bg-brand-blue/10 rounded-full blur-[100px] animate-blob animation-delay-2000" />
-          
-          {/* Bolha inferior esquerda (Perto da seção About/Projects) */}
-          <div className="absolute bottom-[10%] left-[5%] w-[600px] h-[600px] bg-purple-500/10 rounded-full blur-[130px] animate-blob animation-delay-4000" />
-        </div>
+        <Header />
         <main>{children}</main>
-        <WhatsappButton/>
+        <WhatsappButton />
       </body>
     </html>
   );
